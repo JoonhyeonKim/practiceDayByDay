aeeff9c57bbe530e5c3b9dfd0764f5e10d8f058d
김준현 <joonhyeonkim1015@gmail.com>
2022-12-23 20:37:54 -0500

    Update fraction_sum.py

--- fraction_sum.py
+++ fraction_sum.py
@@ -1,16 +1,14 @@
 def solution(denum1, num1, denum2, num2):
     answer = []
-    if num1 != num2:
-        gcd = 1 # by default, gcd = 1
-        for i in range(1,int(min(num1,num2)+1)):
-            if num1%i==0 and num2%i==0: # Greatest Common Divisor
-                gcd = i
-        num3 = num1*num2
-        n_denum1 = denum1*num2
-        n_denum2 = denum2*num1
-        n_denum3 = n_denum1+n_denum2
-        answer = [n_denum3//gcd, num3//gcd]
-    else:
-        denum3 = denum1+denum2
-        answer = [denum3, num1]
+    num3 = num1*num2
+    n_denum1 = denum1*num2
+    n_denum2 = denum2*num1
+    n_denum3 = n_denum1+n_denum2
+    gcd = 1
+    for i in range(1,(min(num3,n_denum3)+1)):
+        if num3%i==0 and n_denum3%i==0: # Greatest Common Divisor
+            gcd = i
+
+    answer = [n_denum3//gcd, num3//gcd]
+
     return answer
