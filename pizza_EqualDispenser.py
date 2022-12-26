edc44e3cc7ac6f7dde4255cd4c03eaa859af5f72
김준현 <joonhyeonkim1015@gmail.com>
2022-12-25 21:09:51 -0500

    Update pizza_EqualDispenser.py
    I don't know man

--- pizza_EqualDispenser.py
+++ pizza_EqualDispenser.py
@@ -8,3 +8,8 @@ def solution(n):
     answer=lcm/6 # where each pizza is dispensed by 6 pieces, then it is (n/gcd) * (6/gcd)
             
     return answer
+
+# question was, when pizza is sliced by 6 pieces, what are the least amount pizzas(n) that are required for people to have equal piece(s)
+#->
+#thus one needs to do divide by gcd(least)
+#since the amount of chunks one recieves is not something that matters: people/gcd(people,pieces)*pieces/gcd(people,pieces)
