f4ecf1c162618382284255f51476062da383987f
김준현 <joonhyeonkim1015@gmail.com>
2022-12-29 21:22:30 -0500

    Create positionInPlane.py

---
+++ positionInPlane.py
@@ -0,0 +1,13 @@
+def solution(dot):
+    answer = 0
+    x=dot[0]
+    y=dot[1]
+    if x>0 and y>0:
+        answer = 1
+    elif x<0 and y>0:
+        answer = 2
+    elif x<0 and y<0:
+        answer = 3
+    elif x>0 and y<0:
+        answer = 4
+    return answer
