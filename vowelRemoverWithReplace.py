a6b2c83f16ed15bcb88e80cc84d96f0eaf9a25ec
김준현 <joonhyeonkim1015@gmail.com>
2022-12-31 22:23:21 -0500

    Update vowelRemoverWithReplace.py

--- vowelRemoverWithReplace.py
+++ vowelRemoverWithReplace.py
@@ -5,7 +5,7 @@ def solution(my_string):
     
     for i in range(len(v)):
         if v[i] in my_string:
-            mn = mn.replace(v[i], "") ##useful string or list methods: replace, sort, pop, remove, append, while new=sorted(list) also new=mystring.replace("a","b")  
+            mn = mn.replace(v[i], "") ##useful string or list methods: replace, sort, pop, remove, append, while new=sorted(list) also new=mystring.replace("a","b") https://www.freecodecamp.org/news/how-to-remove-a-specific-character-from-a-string-in-python/ 
     
             
     answer = mn
