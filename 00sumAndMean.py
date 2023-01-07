49d99a00e3d89a29281cbf871900ca2aeb966bef
김준현 <joonhyeonkim1015@gmail.com>
2023-01-07 03:47:03 -0500

    Create 00sumAndMean.py

---
+++ 00sumAndMean.py
@@ -0,0 +1,8 @@
+a,b,c= map(int,input().split(' '))
+print(a+b+c, format((a+b+c)/3,".2f"))
+
+##Good habit for python programming
+##1. split the input string precisely with split method(by space, new line, etc)
+##2. save them to variables with map function's data specification 
+##3. process with variables
+##4. print them with right format with sep
